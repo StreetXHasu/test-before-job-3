c18d3582726c6229c39e39fb61294d0d1b904232
StreetXHasu <nvmihasu@gmail.com>
2021-12-05 20:47:08 +0300

    Готово

--- ExchangeRates.php
+++ ExchangeRates.php
@@ -1,28 +1,163 @@
 <?php
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 class ExchangeRates
 {
-  public $status = 0;
-  public $change = [];
+  /**
+   * @var string Наша пара из двух валют для конвертации
+   */
+  public $pair = '';
+  /**
+   * @var int Количество для конвертации валюты
+   */
+  public int $amount = 1;
+  /**
+   * @var array|void Список доступных пар курсов валют
+   */
+  public $listOfExchange = [];
 
+  /**
+   * @var mixed|string Пара валют для обмена
+   */
+  public $pairRates = '';
+  /**
+   * @var float Курс обмена
+   */
+  public float $rates = 1;
+
+  /**
+   * @var float|int Результат конвертация валют
+   */
+  public float $result = 0;
+
+
+  /**
+   * @param array $arr - Входной массив
+   */
   public function __construct($arr)
   {
-    $this->connect();
-    $this->change = $arr;
+    try {
+
+
+      $this->pair = $this->validateInput($arr);
+      $this->listOfExchange = $this->getListOfExchange();
+      $this->pairRates = $this->getPairRates();
+      $this->rates = $this->pairRates[$this->pair];
+
+      $make =  $this->makeChange($this->rates, $this->amount);
+
+    } catch (Exception $err) {
+      print_r($err->getMessage());
+    }
+    return $make;
   }
 
-  protected function connect()
+  /**
+   * Получения списка доступных валют для конвертации.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function getListOfExchange()
   {
-    if (true) {
-      echo 'подключение ок';
+    try {
+      $client = new GuzzleHttp\Client();
+      $res = $client->request(
+        'GET',
+        'https://currate.ru/api/?get=currency_list&key=' . $_ENV['API_KEY'],
+        ['connect_timeout' => 7]); //connect_timeout в секундах
+      $this->checkConnect($res);
+      $data = json_decode($res->getBody())->data;
+    } catch (Exception $err) {
+      print_r($err->getMessage());
+    }
+    return $data;
+  }
+
+  /**
+   * Получаем курс валюты для текущей пары.
+   *
+   * @return mixed
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function getPairRates()
+  {
+    try {
+
+      //Проверка на наличие такой пары валют в списке
+      if (!in_array($this->pair,$this->listOfExchange)){
+          throw new Exception('В базе нет доступного обмена для таких валют '.$this->pair);
+      }
+
+
+      $client = new GuzzleHttp\Client();
+      $res = $client->request(
+        'GET',
+        'https://currate.ru/api/?get=rates&pairs=' . $this->pair . '&key=' . $_ENV['API_KEY'],
+        ['connect_timeout' => 7]); //connect_timeout в секундах
+      $this->checkConnect($res);
+      $data = json_decode($res->getBody(), true)['data'];
+    } catch (Exception $err) {
+      print_r($err->getMessage());
+    }
+    return $data;
+  }
+
+  /**
+   * Проверяем подключение к серверу API.
+   *
+   * @param $client
+   * @throws Exception
+   */
+  protected function checkConnect($client)
+  {
+    if ($client->getStatusCode() !== 200) {
+      throw new Exception('Сервер не ответил. Код: ' . $client->getStatusCode());
+    }
+    $check = json_decode($client->getBody());
+    if (intval($check->status) !== 200) { //я чёт не понял, почему возвращает статус в строке то в числе, поэтому конвертим. Странный апи
+      throw new Exception(
+        'Сервер вернул ошибку. Код: ' . $check->status .
+        ' и сказал: ' . $check->message
+      );
     }
   }
 
-  protected function makeChange()
+  /**
+   * Простой метод для проверки входных данных.
+   *
+   * @param array $arr
+   * @return string
+   * @throws Exception
+   */
+  protected function validateInput($arr): string
   {
-    if (true) {
-      echo 'подключение ок';
+    if (!$arr['from']) {
+      throw new Exception('Неправильные входные данные. Нет валюты ИЗ которой происходит конвертация');
     }
+    if (!$arr['to']) {
+      throw new Exception('Неправильные входные данные. Нет валюты В которую происходит конвертация');
+    }
+    if (!$arr['amount']) {
+      throw new Exception('Неправильные входные данные. Не указана сумма, которую вы хотите поменять.');
+    }
+
+    $this->amount = $arr['amount'];
+    return $arr['from'] . $arr['to'];
+  }
+
+  /**
+   * Делаем обмен валюты посредством сложного вычисления.
+   *
+   * @param $rate
+   * @param $amount
+   * @return float|int
+   */
+  protected function makeChange($rate, $amount)
+  {
+    $this->result = $rate * $amount;
+    return $this->result;
   }
 
 }
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-
 require_once "ExchangeRates.php";
 
-$change = [['USD' => 123, 'rub']];
-$newExcgange = new ExchangeRates($change);
+try {
+  $change = ['from' => 'USD', 'to' => 'RUB', 'amount' => 50];
+  $newExcgange = new ExchangeRates($change);
 
-var_dump($newExcgange);
+  echo "Обмен успешно совершён! <br>";
+  echo "Результат: $change[amount] $change[from] = <b>$newExcgange->result</b> $change[to] <br>";
+  echo "По курсу 1 $change[from] = <b>$newExcgange->rates</b> $change[to] <br>";
+} catch (Exception $err) {
+  print_r($err->getMessage());
+}
